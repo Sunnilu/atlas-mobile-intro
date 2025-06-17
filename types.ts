3c6479834041cb105f65e2e97d2972823963e2bc
Suntha <Suntha.Lucas@atlasschool.com>
2025-06-16 20:39:19 -0500

    RootStactParamList

---
+++ types.ts
@@ -0,0 +1,10 @@
+// types.ts
+export type RootStackParamList = {
+  Home: undefined;
+  AddActivity: undefined; // Or { someParam: string } if it takes params
+};
+export type TabParamList = {
+  Home: undefined;
+  Explore: undefined;
+  Settings: undefined;
+};
