375d0da523384bb6aa5e9f6707fa5a805cc4b4b1
Suntha <Suntha.Lucas@atlasschool.com>
2025-06-18 09:02:08 -0500

    updates

--- app/sqlite-test.tsx
+++ app/sqlite-test.tsx
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react';
+import * as React from 'react';
+import { useEffect } from 'react';
 import { View, Text } from 'react-native';
 import * as SQLite from 'expo-sqlite';
 
 export default function TestSQLite() {
   useEffect(() => {
-    const db = SQLite.openDatabase('test.db');
-    db.transaction(tx => {
+    const db = SQLite.openDatabaseSync('test.db');
+    db.transaction((tx: { executeSql: (arg0: string) => void; }) => {
       tx.executeSql('CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY NOT NULL);');
     });
   }, []);
