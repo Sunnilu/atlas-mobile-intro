b92141fa66645d13457eb8bb66233d9f0319a9df
Suntha <Suntha.Lucas@atlasschool.com>
2025-06-26 09:56:30 -0500

    declare expo sqlite/next

--- types/sqlite.d.ts
+++ types/sqlite.d.ts
@@ -1,27 +1,29 @@
-// types/sqlite.d.ts
+// // types/sqlite.d.ts
 
-export interface ResultSet {
-  insertId?: number;
-  rowsAffected: number;
-  rows: {
-    length: number;
-    item: (index: number) => any;
-    _array: any[];
-  };
-}
+// export interface ResultSet {
+//   insertId?: number;
+//   rowsAffected: number;
+//   rows: {
+//     length: number;
+//     item: (index: number) => any;
+//     _array: any[];
+//   };
+// }
 
-export interface SQLTransaction {
-  executeSql(
-    sqlStatement: string,
-    args?: any[],
-    callback?: (tx: SQLTransaction, result: ResultSet) => void,
-    errorCallback?: (tx: SQLTransaction, error: any) => boolean
-  ): void;
-}
-export interface SQLDatabase {
-  transaction(
-    callback: (tx: SQLTransaction) => void,
-    errorCallback?: (error: any) => void,
-    successCallback?: () => void
-  ): void;
-}
+// export interface SQLTransaction {
+//   executeSql(
+//     sqlStatement: string,
+//     args?: any[],
+//     callback?: (tx: SQLTransaction, result: ResultSet) => void,
+//     errorCallback?: (tx: SQLTransaction, error: any) => boolean
+//   ): void;
+// }
+// export interface SQLDatabase {
+//   transaction(
+//     callback: (tx: SQLTransaction) => void,
+//     errorCallback?: (error: any) => void,
+//     successCallback?: () => void
+//   ): void;
+// }
+// types/expo-sqlite-next.d.ts
+declare module 'expo-sqlite/next';
